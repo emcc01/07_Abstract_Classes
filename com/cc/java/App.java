36843c6fda32126bb01c96b166e9ecdff94a19b9
Eva <e.moritz.comcave@gmail.com>
2023-05-30 12:26:07 +0200

    rechnen fertig

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,8 +4,16 @@ public class App {
     
     public static void main(String[] args) {
      
-       
-    }
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Triangle triangle = new Triangle(10.0, 10.0);
+        Circle circle = new Circle(5.0);
+
+        output("------ Flächen ------");
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Dreieck: " + triangle.area());
+        output("Fläche Kreis: " + circle.area());
+            
+            }
 
   
 
